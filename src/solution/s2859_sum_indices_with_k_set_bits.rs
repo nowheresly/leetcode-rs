b4d56e855d575ab3e12c8513146d4a87ea08e8ac
Sylvere Richard <sylvere.richard@gmail.com>
2026-07-21 22:22:58 +0200

    fix warnings

--- src/solution/s2859_sum_indices_with_k_set_bits.rs
+++ src/solution/s2859_sum_indices_with_k_set_bits.rs
@@ -5,7 +5,7 @@ impl Solution {
     pub fn sum_indices_with_k_set_bits(nums: Vec<i32>, k: i32) -> i32 {
         let mut res = 0;
         for i in 0..nums.len() {
-            let mut bits = usize::count_ones(i) as i32;
+            let bits = usize::count_ones(i) as i32;
             if bits == k {
                 res += nums[i];
             }
